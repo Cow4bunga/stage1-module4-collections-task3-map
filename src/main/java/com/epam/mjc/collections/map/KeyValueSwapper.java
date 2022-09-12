2f60e374f7e4fb693b1c820ab943d167cce57808
Ivan Levitsky <yavanyaprivet@gmail.com>
2022-09-12 23:01:42 +0300

    done

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -1,8 +1,20 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class KeyValueSwapper {
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
+        Map<String,Integer> hashMap=new HashMap<>();
+        for(Map.Entry<Integer,String> entry:sourceMap.entrySet()){
+            if(hashMap.containsKey(entry.getValue())){
+                if(hashMap.get(entry.getValue())>entry.getKey()){
+                    hashMap.put(entry.getValue(),entry.getKey());
+                }
+            }else{
+                hashMap.put(entry.getValue(),entry.getKey());
+            }
+        }
+        return hashMap;
     }
 }
--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,26 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        if(sentence.isEmpty())
+            return new HashMap<>();
+
+        Map<String, Integer> hMap = new HashMap<>();
+        String words = sentence.toLowerCase(Locale.ROOT);
+        String[] tokens = words.split("[ \\.,]+");
+        System.out.println(words);
+
+        for (String item : tokens) {
+            if (hMap.containsKey(item)) {
+                hMap.put(item, hMap.get(item) + 1);
+            } else {
+                hMap.put(item, 1);
+            }
+        }
+        return hMap;
     }
 }
